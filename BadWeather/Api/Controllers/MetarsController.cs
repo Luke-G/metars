baa5ae9f9111cd590b274f0ed92a56a93a773f8e
Luke Gorman <luke@lukegorman.co.uk>
2022-08-28 19:13:54 +1000

    feat: get metars by wind and visibility, filterable with an ICAO query param for region filters

--- BadWeather/Api/Controllers/MetarsController.cs
+++ BadWeather/Api/Controllers/MetarsController.cs
@@ -16,14 +16,42 @@ public class MetarsController : ControllerBase
     }
 
     [HttpGet("gust")]
-    public async Task<ActionResult<List<Metar>>> GetTop20Gusts()
+    public async Task<ActionResult<List<Metar>>> GetTop20Gusts([FromQuery] string icaoPrefix)
     {
-        IList<Metar> metars = await _metarProvider.RetrieveMetars();
+        IList<Metar> allMetars = await _metarProvider.RetrieveMetars();
 
-        IEnumerable<Metar> highestGustMetars = metars
+        IEnumerable<Metar> metars = allMetars
             .OrderByDescending(q => q.WindGustKnots)
+            .Where(q => q.StationIcao.StartsWith(icaoPrefix.ToUpperInvariant()))
             .Take(20);
 
-        return Ok(highestGustMetars.ToList());
+        return Ok(metars.ToList());
+    }
+    
+    [HttpGet("wind")]
+    public async Task<ActionResult<List<Metar>>> GetTop20Wind([FromQuery] string icaoPrefix)
+    {
+        IList<Metar> allMetars = await _metarProvider.RetrieveMetars();
+
+        IEnumerable<Metar> metars = allMetars
+            .OrderByDescending(q => q.WindSpeedKnots)
+            .Where(q => q.StationIcao.StartsWith(icaoPrefix.ToUpperInvariant()))
+            .Take(20);
+
+        return Ok(metars.ToList());
+    }
+    
+    [HttpGet("lowvisibility")]
+    public async Task<ActionResult<List<Metar>>> GetTop20LowVisibility([FromQuery] string icaoPrefix)
+    {
+        IList<Metar> allMetars = await _metarProvider.RetrieveMetars();
+
+        IEnumerable<Metar> metars = allMetars
+            .Where(q => q.Visibility != null)
+            .Where(q => q.StationIcao.StartsWith(icaoPrefix.ToUpperInvariant()))
+            .OrderBy(q => q.Visibility)
+            .Take(20);
+
+        return Ok(metars.ToList());
     }
 }
