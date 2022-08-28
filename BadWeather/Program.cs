ce30fb02b9466f5907c07386882a7ab003bb589b
Luke Gorman <luke@lukegorman.co.uk>
2022-08-28 20:53:06 +1000

    chore: enable swagger in production

--- BadWeather/Program.cs
+++ BadWeather/Program.cs
@@ -32,11 +32,8 @@ builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
