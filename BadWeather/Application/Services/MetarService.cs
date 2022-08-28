40633492efad2f980fd42596ea6ca3106634f523
Luke Gorman <luke@lukegorman.co.uk>
2022-08-28 20:58:38 +1000

    chore: add memory cache for metar access (until IDistributedCache is added)

--- BadWeather/Application/Services/MetarService.cs
+++ BadWeather/Application/Services/MetarService.cs
@@ -1,41 +1,58 @@
 ﻿using BadWeather.Application.Contracts;
 using BadWeather.Application.Filters;
 using BadWeather.Domain.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace BadWeather.Application.Services;
 
 public class MetarService : IMetarService
 {
     private readonly IMetarProvider _metarProvider;
+    private readonly IMemoryCache _memoryCache;
 
-    public MetarService(IMetarProvider metarProvider)
+    public MetarService(IMetarProvider metarProvider, IMemoryCache memoryCache)
     {
         _metarProvider = metarProvider;
+        _memoryCache = memoryCache;
     }
 
     public async Task<IEnumerable<Metar>> GetStorms()
     {
-        IList<Metar> allMetars = await _metarProvider.RetrieveMetars();
+        IEnumerable<Metar> allMetars = await GetMetars();
         return allMetars.WhereStormy();
     }
 
     public async Task<IEnumerable<Metar>> GetHighestGusts()
     {
-        IList<Metar> allMetars = await _metarProvider.RetrieveMetars();
+        IEnumerable<Metar> allMetars = await GetMetars();
         return allMetars.OrderByDescending(q => q.WindGustKnots);
     }
 
     public async Task<IEnumerable<Metar>> GetHighestWinds()
     {
-        IList<Metar> allMetars = await _metarProvider.RetrieveMetars();
+        IEnumerable<Metar> allMetars = await GetMetars();
         return allMetars.OrderByDescending(q => q.WindSpeedKnots);
     }
 
     public async Task<IEnumerable<Metar>> GetLowestVisibility()
     {
-        IList<Metar> allMetars = await _metarProvider.RetrieveMetars();
+        IEnumerable<Metar> allMetars = await GetMetars();
         return allMetars
             .Where(q => q.Visibility != null)
             .OrderBy(q => q.Visibility);
+    }
+
+    private async Task<IEnumerable<Metar>> GetMetars()
+    {
+        _memoryCache.TryGetValue("metars", out IList<Metar> cachedMetars);
+
+        if (cachedMetars is not null)
+            return cachedMetars;
+        
+        IList<Metar> metars = await _metarProvider.RetrieveMetars();
+        
+        _memoryCache.Set("metars", metars, TimeSpan.FromMinutes(5));
+
+        return metars;
     }
 }
--- BadWeather/Program.cs
+++ BadWeather/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
 builder.Services.AddPersistence(builder.Configuration);
 
