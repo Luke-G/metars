a6ee03b4595abf328bafe0550acef06a70ad29ca
Luke Gorman <luke@lukegorman.co.uk>
2022-08-28 18:58:59 +1000

    chore: make ID and text fields required on Metar DbContext

--- BadWeather/Infrastructure/Database/MetarEntityTypeConfiguration.cs
+++ BadWeather/Infrastructure/Database/MetarEntityTypeConfiguration.cs
@@ -9,7 +9,15 @@ public class MetarEntityTypeConfiguration : IEntityTypeConfiguration<Metar>
     public void Configure(EntityTypeBuilder<Metar> builder)
     {
         builder
+            .Property(m => m.Id)
+            .IsRequired();
+
+        builder
             .Property(m => m.StationIcao)
+            .IsRequired();
+
+        builder
+            .Property(m => m.Text)
             .IsRequired();
     }
 }
